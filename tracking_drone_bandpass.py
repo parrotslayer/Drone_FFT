525187b430e1e75b13b6469343b97e180f5f3036
parrotslayer <squigglydouche@gmail.com>
2017-09-26 13:53:54 +1000

    Got diagnoistics up for tracking reference tone

--- tracking_drone_bandpass.py
+++ tracking_drone_bandpass.py
@@ -77,19 +77,21 @@ while(loop < 5):
     index2freq = 1.0/(2.0*T)/(N/2)
     minF = 8000      #min freq Hz
     maxF = 9000     #max freq Hz
+    HpassFreq = 1000     #ignore values below this freq
+    Hpass = round(HpassFreq/index2freq)
     index_min = round(minF/index2freq)
     index_max = round(maxF/index2freq)
-    NF_L = np.average(psd_L[10:])    # Find the noise floor
-    NF_R = np.average(psd_R[10:])
+    NF_L = np.average(psd_L[Hpass:])    # Find the noise floor
+    NF_R = np.average(psd_R[Hpass:])
     SNR = 1.2     # gain, not in dB. Using a 3dB SNR
-    peak_height = (NF_L+NF_R)/2*SNR     #calc the min height for a signal
+    peak_height_L = NF_L*SNR     #calc the min height for a signal
+    peak_height_R = NF_R*SNR     #calc the min height for a signal
 
-    print(peak_height)
-    testnum = 2
+    testnum = 6
     plt.figure()
-    NF1, = plt.plot(xf[10:],psd_L[10:],label="Raw Data")
-    NF2, = plt.plot((0,Fs/2),(peak_height, peak_height),'r--',label = "Noise Floor")
-    NF3, = plt.plot((0,Fs/2),(peak_height*SNR, peak_height*SNR),'k-',label = "Minimum Signal")
+    NF1, = plt.plot(xf[Hpass:],psd_L[Hpass:],label="Raw Data")
+    NF2, = plt.plot((0,Fs/2),(peak_height_L, peak_height_L),'r--',label = "Noise Floor")
+    NF3, = plt.plot((0,Fs/2),(peak_height_L*SNR, peak_height_L*SNR),'k-',label = "Minimum Signal")
     NF4, = plt.plot( (minF,minF), (0,max(psd_L[10:])), label = "Min Frequency" )
     NF5, = plt.plot( (maxF,maxF), (0,max(psd_L[10:])), label = "Max Frequency" )
     plt.legend(handles = [NF1,NF2,NF3,NF4,NF5])
@@ -108,8 +110,8 @@ while(loop < 5):
     from detect_peaks import detect_peaks
 
     # detect peaks and show the m on a plot
-    ind_L = detect_peaks(psd_L, mph=peak_height, mpd=1, show=False)
-    ind_R = detect_peaks(psd_R, mph=peak_height, mpd=1, show=False)
+    ind_L = detect_peaks(psd_L, mph=peak_height_L, mpd=1, show=False)
+    ind_R = detect_peaks(psd_R, mph=peak_height_R, mpd=1, show=False)
 
     # Peak Filtering
 
@@ -131,15 +133,10 @@ while(loop < 5):
     if len(peaks_L_amp) > 0 and len(peaks_R_amp) > 0:
         # find difference in peaks
         peak_diff = peaks_L_amp[0] - peaks_R_amp[0]
-        print(peak_diff)
-
-        # plot FFT of both chanels
-        ind_L = detect_peaks(psd_L, mph=peak_height, mpd=1, show=False)
-        ind_R = detect_peaks(psd_R, mph=peak_height, mpd=1, show=False)
-
+        # Print and plot Differentials
+        print('Loop {0}'.format(loop))
         print('Frequency = {0} \n Amplitude = {1}'.format(xf[ind_L],psd_L[ind_L]))
 
-        # Print Differentials
         plt.figure()
         left_plot, = plt.plot(freqs, psd_L, label="psd_L")
         plt.hold(True)
